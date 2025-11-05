a9166f41db9a833f442d689e5382af2eedf5a8b8
lollobrollo <lorenzo.bortolussi03@gmail.com>
2025-11-05 18:16:26 +0100

    changed operator<< from virtual method to friend function; removed constructors from abstract class

--- hw1/sparse.hpp
+++ hw1/sparse.hpp
@@ -18,19 +18,12 @@ Proposals:
 
 class SparseMatrix {
   public:
-    virtual SparseMatrix(const uint nrows, const uint ncols) = 0; // constructor
-    virtual SparseMatrix(const SparseMatrix& other) = 0; // copy constructor
-    virtual SparseMatrix& operator=(const SparseMatrix& other) = 0; // copy assignment
-    virtual ~SparseMatrix() = 0; // destructor
-    
     virtual const double& operator()(const uint row, const uint col) const = 0; // read only (for const objects)
     virtual double& operator()(const uint row, const uint col) = 0; // write access (for non const objects)
     virtual SparseMatrix operator*(const SparseMatrix& mat, const SparseMatrix& vec) const = 0; // dot product
     virtual SparseMatrix operator*(const SparseMatrix& mat, const std::vector<double>& vec) const = 0; // dot product
-    virtual std::ostream& operator<<(std::ostream& os, const SparseMatrix& mat) = 0; // custom output for pretty printing
-
     virtual const uint get_nrows() const = 0;
     virtual const uint get_ncols() const = 0;
     virtual const uint get_nonzeros() const = 0;
-    
+    friend std::ostream& operator<<(std::ostream& os, const SparseMatrixCOO& mat);
 };
